01422203f94981f36711768a7a8f491858b1dbe9
riyad <riyad>
2017-03-27 16:41:04 +0300

    add findOne Test

--- src/main/java/com/example/rest/ProduitRestService.java
+++ src/main/java/com/example/rest/ProduitRestService.java
@@ -3,6 +3,7 @@ package com.example.rest;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +20,11 @@ public class ProduitRestService {
 	@RequestMapping(value="/produits", method = RequestMethod.GET)
 	public List<Produit> listeProduit(){
 		return produitRepository.findAll(); 
+	}
+	
+	@RequestMapping(value="/produits/{id}", method = RequestMethod.GET)
+	public Produit getProduit(@PathVariable("id") Long id){
+		return produitRepository.findOne(id); 
 	}
 
 }
