8f7c1dd6b85110eb0fe5f972b0f110052b76d09f
riyad <riyad>
2017-03-27 14:01:54 +0300

    first commit

---
+++ src/main/java/com/example/SpringBootMsAngularjsApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMsAngularjsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMsAngularjsApplication.class, args);
+	}
+}
