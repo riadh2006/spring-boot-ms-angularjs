cbb81760c253ea97a4a483db285df84e17d3c555
riyad <riyad>
2017-03-27 16:15:25 +0300

    add JPA Repository and make simple test

--- src/main/java/com/example/SpringBootMsAngularjsApplication.java
+++ src/main/java/com/example/SpringBootMsAngularjsApplication.java
@@ -1,12 +1,26 @@
 package com.example;
 
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import com.example.dao.ProduitRepository;
+import com.example.entities.Produit;
 
 @SpringBootApplication
 public class SpringBootMsAngularjsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootMsAngularjsApplication.class, args);
+		ApplicationContext ctx = SpringApplication.run(SpringBootMsAngularjsApplication.class, args);
+		ProduitRepository produitRepository = ctx.getBean(ProduitRepository.class);
+		produitRepository.save(new Produit("tomate", 10, 1));
+		produitRepository.save(new Produit("potate", 12, 21));
+		produitRepository.save(new Produit("chocolate", 8, 43));
+		
+		List<Produit> produits = produitRepository.findAll();
+		// Loop with lambda expression 
+		produits.forEach(pr -> System.out.println(pr.getDescription()));
 	}
 }
--- src/main/java/com/example/entities/Produit.java
+++ src/main/java/com/example/entities/Produit.java
@@ -7,44 +7,63 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
-public class Produit implements Serializable{
-	
+public class Produit implements Serializable {
+
 	/**
 	 * @author Riadh BEL HADJ SALAH
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue
-	private Long id; 
-	private String description; 
-	private double price; 
+	private Long id;
+	private String description;
+	private double price;
 	private int quantity;
-	
+
 	public Produit() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-	
+
+	public Produit(String description, double price, int quantity) {
+		super();
+		this.description = description;
+		this.price = price;
+		this.quantity = quantity;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
 	public String getDescription() {
 		return description;
 	}
+
 	public void setDescription(String description) {
 		this.description = description;
 	}
+
 	public double getPrice() {
 		return price;
 	}
+
 	public void setPrice(double price) {
 		this.price = price;
 	}
+
 	public int getQuantity() {
 		return quantity;
 	}
+
 	public void setQuantity(int quantity) {
 		this.quantity = quantity;
-	} 
-	
+	}
 
 }
