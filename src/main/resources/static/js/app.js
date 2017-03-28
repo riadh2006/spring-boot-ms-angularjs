eeddda499fe9a7f530435470c4349f6ce09b7594
riyad <riyad>
2017-03-28 13:35:47 +0300

    add search and pagination to angular app

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,14 +1,26 @@
 var app = angular.module("product-app", []);
 app.controller("prod-ctrl", function($scope, $http) {
 	$scope.produits = null;
-	$http.get("http://localhost:8080/findProduct?field=ta")
-	.then(function onSuccess(response) {
-		// Handle success
-		$scope.produits = response.status;
-	}, function onError(response) {
-		// Handle error
-		var data = response.data;
-		console.log(data);
-	});
+	$scope.motCLe = ""; 
+	$scope.currentPage = 0; 
+	$scope.size = 2; 
+	
+	$scope.chercherProduits = function (){
+		
+//		$http.get("http://localhost:8080/findProduct?field="+$scope.motCLe);
+		$http.get("http://localhost:8080/findProduct?field="+$scope.motCLe
+				+"&page=" + $scope.currentPage
+				+"&size=" + $scope.size		
+		)
+		.then(function onSuccess(response) {
+			// Handle success
+			$scope.produits = response.data;
+		}, function onError(response) {
+			// Handle error
+			var data = response.data;
+			console.log(data);
+		});
+		
+	}; 
 
 });
