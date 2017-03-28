038b45c42186534bee6d4cfa49b06e8ecb21b737
riyad <riyad>
2017-03-28 11:32:32 +0300

    Pagination with find by field

--- src/main/java/com/example/dao/ProduitRepository.java
+++ src/main/java/com/example/dao/ProduitRepository.java
@@ -1,9 +1,16 @@
 package com.example.dao;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.example.entities.Produit;
 
 public interface ProduitRepository extends JpaRepository<Produit, Long>{
+	
+	@Query("select p from Produit p where p.description like :f")
+	Page<Produit> findProduct(@Param("f") String field, Pageable pageable); 
 
 }
--- src/main/java/com/example/rest/ProduitRestService.java
+++ src/main/java/com/example/rest/ProduitRestService.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.dao.ProduitRepository;
@@ -16,41 +17,49 @@ import com.example.entities.Produit;
 
 @RestController
 public class ProduitRestService {
-	
+
 	@Autowired
 	private ProduitRepository produitRepository;
-	
-	@RequestMapping(value="/produits", method = RequestMethod.GET)
-	public List<Produit> listeProduit(){
-		return produitRepository.findAll(); 
+
+	@RequestMapping(value = "/produits", method = RequestMethod.GET)
+	public List<Produit> listeProduit() {
+		return produitRepository.findAll();
 	}
 
 	// List products by page
-	@RequestMapping(value="/produits-pages", method = RequestMethod.GET)
-	public Page<Produit> listeProduitByPage(int page, int size){
-		return produitRepository.findAll(new PageRequest(page, size)); 
-	}
-	
-	@RequestMapping(value="/produits/{id}", method = RequestMethod.GET)
-	public Produit save(@PathVariable("id") Long id){
-		return produitRepository.findOne(id); 
-	}
-	
-	
-	@RequestMapping(value="/produits", method = RequestMethod.POST)
-	public Produit save(@RequestBody Produit produit){
-		return produitRepository.save(produit); 
+	@RequestMapping(value = "/produits-pages", method = RequestMethod.GET)
+	public Page<Produit> listeProduitByPage(int page, int size) {
+		return produitRepository.findAll(new PageRequest(page, size));
 	}
 
-	@RequestMapping(value="/produits/{id}", method = RequestMethod.PUT)
-	public Produit update(@RequestBody Produit produit, @PathVariable("id") Long id){
+	// List products by page
+	@RequestMapping(value = "/findProduct", method = RequestMethod.GET)
+	public Page<Produit> listeProduitByPageByField(
+			String field, 
+			@RequestParam(name="page", defaultValue="0") int page, 
+			@RequestParam(name="size", defaultValue="5") int size) {
+		return produitRepository.findProduct("%" + field + "%", new PageRequest(page, size));
+	}
+
+	@RequestMapping(value = "/produits/{id}", method = RequestMethod.GET)
+	public Produit save(@PathVariable("id") Long id) {
+		return produitRepository.findOne(id);
+	}
+
+	@RequestMapping(value = "/produits", method = RequestMethod.POST)
+	public Produit save(@RequestBody Produit produit) {
+		return produitRepository.save(produit);
+	}
+
+	@RequestMapping(value = "/produits/{id}", method = RequestMethod.PUT)
+	public Produit update(@RequestBody Produit produit, @PathVariable("id") Long id) {
 		produit.setId(id);
-		return produitRepository.saveAndFlush(produit); 
+		return produitRepository.saveAndFlush(produit);
 	}
 
-	@RequestMapping(value="/produits/{id}", method = RequestMethod.DELETE)
-	public void delete(@PathVariable("id") Long id){
-		produitRepository.delete(id); 
+	@RequestMapping(value = "/produits/{id}", method = RequestMethod.DELETE)
+	public void delete(@PathVariable("id") Long id) {
+		produitRepository.delete(id);
 	}
 
 }
